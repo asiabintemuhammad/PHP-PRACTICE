52bc1c65625528d4cbac6db60e0f06d8e7bda346
Asia Binte Muhammad <asiabintemuhammad48@gmail.com>
2026-07-08 22:17:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="title" content="Web Dev With Asia ">
+	<meta name="description" content="full stack web development practice">
+	<meta name="keywords" content="life, music , travel">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="3 days">
+	<title>PHP</title>
+	<!-- font links -->
+		
+	<!--css link-->	
+
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">-->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/media.css">
+
+<body>
+	<h1> lorem ipsum dot amet </h1>
+	<div class="container">
+		<?php
+		echo "Hello, I am learning PHP!";
+		?>
+	</div>
+
+
+
+	<!--js link-->
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
